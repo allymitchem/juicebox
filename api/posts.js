1f6e5585a280f78cacc1d2f5cf855f56c6296eb1
Allyson Mitchem <allysonmitchem@gmail.com>
2022-11-01 10:12:21 -0600

    working on writing /api/posts, needing try cdoe

--- api/posts.js
+++ api/posts.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const postsRouter = express.Router();
-const {getAllPosts} = require('../db');
+const {getAllPosts, createPost} = require('../db');
+const {requireUser} = require('./utils');
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  // res.send({message: 'under construction' });
+  const {title, content, tags = ""} = req.body;
+
+  const tagArr= tags.trim().split(/\s+/)
+  const postData = {};
+
+  if (tagArr.length){
+    postData.tags = tagArr;
+  }
+
+  try {
+
+  } catch ({name, message}){
+    next({ name, message})
+  }
+});
 
 postsRouter.use((req, res, next)=>{
   console.log("A request is being made to /posts");
   next();
 
 });
-
-postsRouter.get ('/', async (req,res)=>{
+ 
+postsRouter.get ('/',async (req,res)=>{
   const posts = await getAllPosts();
   res.send({
     posts
