8fc8d1c523ef43924eab91aab6bf39cc946ba5ed
Allyson Mitchem <allysonmitchem@gmail.com>
2022-11-01 12:23:44 -0600

    fixed error with /api/posts

--- api/posts.js
+++ api/posts.js
@@ -6,15 +6,29 @@ const {requireUser} = require('./utils');
 postsRouter.post('/', requireUser, async (req, res, next) => {
   // res.send({message: 'under construction' });
   const {title, content, tags = ""} = req.body;
-
   const tagArr= tags.trim().split(/\s+/)
-  const postData = {};
+  
+  
+  const postData = {authorId: req.user.id, title, content};
+  console.log(postData, 'BIG FAT NOTE')
 
   if (tagArr.length){
     postData.tags = tagArr;
   }
 
   try {
+    const post = await createPost(postData)
+    console.log(post)
+    if (post){
+      res.send({post});
+    } else {
+      next({
+        name: 'NoPostError',
+        message: 'Valid post is necessary'
+      });
+    }
+
+    
 
   } catch ({name, message}){
     next({ name, message})
